71830b62010ed8036229ccceaa572d84593265bd
Abhinn Krishn <abhinnk21@gmail.com>
2024-02-09 23:10:03 +0530

    added meta

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Will you be my Valentine",
-  description: "click on the LINK!!!",
+  title: "Will you be my Valentine???",
+  description: "CLICK ON THE LINK!!!",
 };
 
 export default function RootLayout({ children }) {
